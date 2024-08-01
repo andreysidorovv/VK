152fe3d7c0b8a69c3cf859d597ce101e6ebd29c2
Андрей Сидоров <sidorov.andrey2005@mail.ru>
2024-08-01 14:20:56 +0300

    profile assignment

---
+++ fromFormToServer.test.js
@@ -0,0 +1,49 @@
+const fromFormToServer = require('./fromFormToServer');
+
+test('отечественное физическое лицо', () => {
+    const input = { isForeign: false, isJuridical: false, title: 'Иван Иванов', tin: '1234567890' };
+    const output = fromFormToServer(input);
+    expect(output).toEqual({
+        type: 'physical',
+        tin: '1234567890',
+        name: 'Иван Иванов',
+        foreign_tin: null,
+        company_title: null,
+    });
+});
+
+test('отечественное юридическое лицо', () => {
+    const input = { isForeign: false, isJuridical: true, title: 'ООО Ромашка', tin: '0987654321' };
+    const output = fromFormToServer(input);
+    expect(output).toEqual({
+        type: 'juridical',
+        tin: '0987654321',
+        name: null,
+        foreign_tin: null,
+        company_title: 'ООО Ромашка',
+    });
+});
+
+test('иностранное физическое лицо', () => {
+    const input = { isForeign: true, isJuridical: false, title: 'Джон Доу', tin: '1234567890' };
+    const output = fromFormToServer(input);
+    expect(output).toEqual({
+        type: 'foreign_physical',
+        tin: null,
+        name: 'Джон Доу',
+        foreign_tin: '1234567890',
+        company_title: null,
+    });
+});
+
+test('иностранное юридическое лицо', () => {
+    const input = { isForeign: true, isJuridical: true, title: 'Global Inc', tin: '0987654321' };
+    const output = fromFormToServer(input);
+    expect(output).toEqual({
+        type: 'foreign_juridical',
+        tin: null,
+        name: null,
+        foreign_tin: '0987654321',
+        company_title: 'Global Inc',
+    });
+});
